0af3a72cf8bf445073e4dfbc4504154d85310dd2
PiyushD30 <133135876+PiyushD30@users.noreply.github.com>
2023-05-11 00:31:45 +0530

    Add files via upload

---
+++ then_catch.js
@@ -0,0 +1,18 @@
+let cfipromise = new Promise((resolve, reject) => {
+    const prompt= require("prompt-sync")()
+    const a= prompt("Enter 1st Number:")
+    const b= prompt("Enter 2nd Number:")
+   if (a % 2 == 0 && b % 2 ==0 )
+     resolve("success")
+  else
+     reject("error")
+ })
+ 
+ cfipromise
+   .then((result) => {
+     console.log(result)
+   }) 
+   .catch((error) => {
+     console.log(error)
+   })
+ 
